a3a2a66ae07e4d24f27b0ad395ffcfa73099fc99
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-07-17 15:20:03 -0700

    leetcode

--- 287.FindtheDuplicateNumber.cpp
+++ 287.FindtheDuplicateNumber.cpp
@@ -10,85 +10,23 @@ using namespace std;
 class Solution
 {
 public:
-    // int findDuplicate(vector<int>& nums)
-    // {
-    //     const int range_low = 1;
-    //     const int range_high = nums.size()-1;
-    //     for (int i = 0; i < nums.size(); i++)
-    //     {
+    int findDuplicate(vector<int>& nums)
+    {
+        int slow = 0;
+        int fast = 0;
 
-    //     }
-    // }
+        do 
+        {
+            slow = slow[slow];
+            fast = fast[fast];
+        }
+        while (slow != fast);
+        
+    }
 };
 
-// First Attempt : Hashmap o(n) time, not o(1) space WORKS
-
-// int findDuplicate(vector<int> &nums)
-// { // o(n) time, o(n) space
-//     unordered_set<int> values;
-
-//     for (int i = 0; i < nums.size(); i++)
-//     {
-//         // Finds Value Already In Set
-//         if (values.find(nums[i]) != values.end())
-//         {
-//             return nums[i];
-//         }
-//         values.insert(nums[i]);
-//     }
-
-//     return -1;
-// }
-
-// Second Attempt
-
-// int findDuplicate(vector<int> &nums)
-// {
-//     const int range_low = 1;
-//     const int range_high = nums.size() - 1;
-
-    
-//     for (int i = 0; i < nums.size(); i++)
-//     {
-
-//     }
-
-// }
-
-int findDuplicate3(vector<int>& nums)
-{
-	if (nums.size() > 1)
-	{
-		int slow = nums[0]; 
-        cerr  << "Slow: " << slow << endl;
-		int fast = nums[nums[0]]; 
-        cerr << "fast: " << fast << endl;
 
 
-		while (slow != fast)
-		{
-			slow = nums[slow]; 
-            cerr  << "Slow: " << slow << endl;
-			fast = nums[nums[fast]]; 
-            cerr << "fast: " << fast << endl;
-		}
-
-        cerr << "Slow became fast" << endl;
-
-		fast = 0;
-        cerr << fast << endl;
-
-		while (fast != slow)
-		{
-			fast = nums[fast];
-            cerr << "fast: " << fast << endl;
-			slow = nums[slow];
-            cerr << "Slow" << slow << endl;
-		}
-		return slow;
-	}
-	return -1;
-}
 
 int main()
 {
