a7e43231cf344167efdab9ecdbb9d5c3a8f277df
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-07-19 15:24:24 -0700

    Create 238.ProductofArrayExceptSelf.cpp

---
+++ 238.ProductofArrayExceptSelf.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    vector<int> results(nums.size());
+
+    // Prefix Array
+
+    results[0] = 1;
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        results[i] = results[i - 1] * nums[i - 1];
+    }
+
+    // Postfix Array
+    int post_fix = 1;
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        results[i] = results[i] * post_fix;
+        post_fix *= nums[i];
+    }
+
+    return results;
+}
+
+int main()
+{
+    return 0;
+}
